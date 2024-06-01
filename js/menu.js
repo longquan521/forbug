b668244e601d2deb390081110d0b63a301c7795e
longquan521 <569978412@qq.com>
2024-06-01 20:07:11 +0800

    feat: 新增菜单

---
+++ js/menu.js
@@ -0,0 +1,137 @@
+const menuListData = [{
+  menuType: 'main',
+  typeValue: '主食',
+  typeList: [{
+    name: '面',
+    value: 'mian'
+  }, {
+    name: '米',
+    value: 'mi'
+  }, {
+    name: '馒头',
+    value: 'mantou'
+  }, {
+    name: '炒米',
+    value: 'chaomi'
+  }, {
+    name: '小麻花',
+    value: 'xiaomahua'
+  }, {
+    name: '不烂子',
+    value: 'bulanzi'
+  }]
+}, {
+  menuType: 'chaocai',
+  typeValue: '炒菜',
+  typeList: [{
+    name: '糖醋排骨',
+    value: 'tangcupaigu'
+  }, {
+    name: '茄子炒肉',
+    value: 'qiezichaorou'
+  }, {
+    name: '蘑菇炒肉',
+    value: 'moguchaorou'
+  }, {
+    name: '木耳炒肉',
+    value: 'muerchaorou'
+  }, {
+    name: '蒜苔炒肉',
+    value: 'suantaichaorou'
+  }, {
+    name: '土豆丝炒肉',
+    value: 'tudousichaorou'
+  }, {
+    name: '西红柿炒鸡蛋',
+    value: 'xihongshichaojidan'
+  }]
+}, {
+  menuType: 'yuxia',
+  typeValue: '鱼虾',
+  typeList: [{
+    name: '清蒸巴沙鱼',
+    value: 'qingzhengbashayu'
+  }, {
+    name: '煎鲈鱼',
+    value: 'jianluyu'
+  }, {
+    name: '麻辣虾锅',
+    value: 'malaxiaguo'
+  }, {
+    name: '其他',
+    value: 'qita'
+  },]
+}, {
+  menuType: 'jiatingban',
+  typeValue: '家庭版',
+  typeList: [{
+    name: '家庭烤肉',
+    value: 'jiatingkaorou'
+  }, {
+    name: '家庭火锅',
+    value: 'jiatinghuoguo'
+  },]
+}, {
+  menuType: 'xiaochiliangcai',
+  typeValue: '小吃凉菜',
+  typeList: [{
+    name: '花生',
+    value: 'huasheng'
+  }, {
+    name: '毛豆',
+    value: 'maodou'
+  }, {
+    name: '凉拌黄瓜',
+    value: 'liangbanhuanggua'
+  },]
+}, {
+  menuType: 'tanglei',
+  typeValue: '汤类',
+  typeList: [{
+    name: '紫菜蛋花汤',
+    value: 'zicaidanhuatang'
+  }, {
+    name: '银耳莲子粥',
+    value: 'yinerlianzizhou'
+  }, {
+    name: '鱼汤',
+    value: 'yutang'
+  }, {
+    name: '其他',
+    value: 'qita'
+  },]
+}, {
+  menuType: 'chayin',
+  typeValue: '茶饮',
+  typeList: [{
+    name: '金银花茶',
+    value: 'jinyinhuacha'
+  }, {
+    name: '大麦茶',
+    value: 'damaicha'
+  }, {
+    name: '黄连',
+    value: 'huanglian'
+  }, {
+    name: '枸杞',
+    value: 'gouqi'
+  }, {
+    name: '藏红花',
+    value: 'zanghonghua'
+  }, {
+    name: '红茶',
+    value: 'hongcha'
+  }, {
+    name: '绿茶',
+    value: 'lvcha'
+  }, {
+    name: '可乐',
+    value: 'kele'
+  }, {
+    name: '雪碧',
+    value: 'xuebi'
+  }, {
+    name: '柠檬汁',
+    value: 'ningmengzhi'
+  }]
+},];
